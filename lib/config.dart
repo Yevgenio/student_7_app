a114051728ae8f9c3ab5f202c82362e1f19c6c3c
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2024-12-01 04:53:23 +0200

    api ip fix

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
 class Config {
-  static const String apiBaseUrl = 'http://localhost:5000';
-  // static const String apiBaseUrl = 'http://51.84.9.120:5000/api';
+  // static const String apiBaseUrl = 'http://localhost:5000';
+  static const String apiBaseUrl = 'http://51.84.9.120';
 }
